569aa9ebc6814afaf57496bcb008b4913ffff2bc
SaiesShaikh07 <saiesshaikh007@gmail.com>
2024-07-31 09:53:00 +0530

    typeof assignment
    typeof assignment

---
+++ 02-typeOf.js
@@ -0,0 +1,36 @@
+console.log("============= 1 ===============");
+var bankName;
+console.log("Bank Name Is : ", bankName);
+bankName="SBI BankName";
+console.log("Bank Name Is : ", bankName);
+var bankTypeOf = typeof bankName;
+console.log("Bank Name Datatype Is : ", bankTypeOf);
+
+console.log("============= 2 ===============");
+var marks="90%";
+console.log("My Marks Are : ", marks);
+var marksTypeOf=typeof marks;
+console.log("My Marks Datatype Is : ",marksTypeOf);
+
+console.log("============= 3 ===============");
+var isWorking;
+console.log("My Working Status Is  : ", isWorking);
+isWorking=true;
+console.log("My Working Status Is : ", isWorking);
+var WorkingTypeOf = typeof isWorking;
+console.log("isWorking Datatype Is : ", WorkingTypeOf);
+
+console.log("============= 4 ===============");
+
+console.log("--------- a ------------");
+
+var totalCount="Hundred And Seven";
+console.log("Total count is : ",totalCount);
+var totalCountTypeOf=typeof totalCount;
+console.log("Total Count Datatype is : ",totalCountTypeOf);
+
+console.log("--------- b ------------");
+totalCount=107;
+console.log("Total count is : ",totalCount);
+var totalTypeOf = typeof totalCount;
+console.log("Total Count Datatype After updating  : ",totalTypeOf);
