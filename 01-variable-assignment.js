f754c86ab67e57efad9bc353eb24c2ff9cedb663
SaiesShaikh07 <saiesshaikh007@gmail.com>
2024-07-31 07:53:30 +0530

    javascript-variables
    javascript-variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,7 @@
+console.log("My city Name is SANGOLA");
+console.log("My Bestfriend name is SAM");
+console.log("My country name is INDIA");
+console.log("My school name is SANGOLA VIDYAMANDIR SANGOLA");
+console.log("My college name is SANGOLA COLLEGE SANGOLA");
+
+
---
+++ 01-variable.js
@@ -0,0 +1,17 @@
+//variable declaration
+
+var firstName;
+console.log(firstName);
+
+
+// variable initialization
+firstName="saies";
+console.log(firstName);
+
+// variable declaration and initialization
+var lastName="shaikh";
+console.log(lastName);
+
+// variable update
+firstName="JENI";
+console.log(firstName);
