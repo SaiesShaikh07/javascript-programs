ccfb7fad7f5db6b24709b37e332081756005abd4
SaiesShaikh07 <saiesshaikh007@gmail.com>
2024-07-31 09:17:32 +0530

    Variable Assign Assignment
    Variable Assign Assignment

---
+++ 01-variableAssign.js
@@ -0,0 +1,27 @@
+console.log("============= 1 ===============");
+var myFullName;
+console.log("My Full Name Is Before Initialization : ",myFullName);
+myFullName="Saies Shaikh";
+console.log("My First Name and last name After Initialization : ",myFullName);
+
+myFullName="Saies Shafik Shaikh";
+console.log(" after updating My full name is : ",myFullName);
+
+console.log("============= 2 ===============");
+var myLove="I LOVE MYSELF ";
+console.log(myLove);
+console.log("============= 3 ===============");
+var firstName="TONY";
+console.log("My first name is :",firstName);
+var lastName="STARK";
+console.log("My last name is :",lastName);
+var age=25;
+console.log("My Age is :",age);
+var address="L A ";
+console.log("My address is :",address);
+var pinCode=22110033;
+console.log("pincode is :",pinCode);
+var gender="male";
+console.log("Gender  :",gender);
+var isMarried=false;
+console.log("Marriage status :",isMarried);
