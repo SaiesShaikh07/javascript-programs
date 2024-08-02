ed8e334a5acaf7b6d2ca9e036ab4573384b64929
SaiesShaikh07 <saiesshaikh007@gmail.com>
2024-08-02 09:21:24 +0530

    function assignment 2
    function assignment 2

---
+++ 03-funAssig02.js
@@ -0,0 +1,8 @@
+console.log("============= Assignment function 02 ===============");
+function bankDetails(bankName,accountNum,location,pinCode) {
+    console.log("Bank name : ",bankName+" || The Account No. is : ",accountNum+" || Location is : ",location+" || pinCode is :",pinCode);
+}
+bankDetails("CITI Bank", 3456782345, 'Pune', 431202);
+bankDetails("Axis Bank", 7856782345, 'Mumbai', 441202);
+bankDetails("HDFC Bank", 8956782345, 'Pune', 631202, "Open");
+
