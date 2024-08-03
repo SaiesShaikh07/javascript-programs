2994219a8b0718610749c896c3fb9f7f78623d57
SaiesShaikh07 <saiesshaikh007@gmail.com>
2024-08-03 10:05:42 +0530

    String basic assignment
    String basic assignment

---
+++ 03-stringBasicAssign01.js
@@ -0,0 +1,9 @@
+function stringBasics() {
+    console.log(`My dream company is "Google"`);
+    var hobby1="Reading";
+    var hobby2="Editing";
+    var hobby3="solving coding questions";
+    console.log(`My hobbies are : ${hobby1},${hobby2},${hobby3}`);
+    console.log("total sum is  :" ,hobby1.length+hobby2.length+hobby3.length);
+}
+stringBasics();
