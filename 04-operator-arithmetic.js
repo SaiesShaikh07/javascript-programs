1de9d264eb6872c4cedff2e35bf1ad88c43d5459
SaiesShaikh07 <saiesshaikh007@gmail.com>
2024-08-06 09:43:36 +0530

    operators
    operators

---
+++ 04-operator-arithmetic.js
@@ -0,0 +1,20 @@
+console.log(`======== Arithmetic Operators =========`);
+var n1 = 10;
+var n2 = 2;
+var result = n1 + n2;
+console.log(`Addition is : ${result}`);
+var result = n1 - n2;
+console.log(`Subtraction is : ${result}`);
+var result = n1 * n2;
+console.log(`Multiplication is : ${result}`);
+var result = n1 / n2;
+console.log(`Division is : ${result}`);
+
+var result = n1 % n2;
+console.log(`Modules is : ${result}`);
+
+var result = n1 % 4;
+console.log(`Modules is : ${result}`);
+
+var result=n1**n2;
+console.log(`exponential is : ${result}`);
---
+++ 04-operator-comparision.js
@@ -0,0 +1,11 @@
+var n1="50";
+var n2=50;
+console.log(n1==n2);
+console.log(n1===n2);//it will check the value with type
+
+
+var n1=10;
+var n2=20;
+console.log(n1!=n2);
+console.log(n1>n2);
+console.log(n1<n2);
