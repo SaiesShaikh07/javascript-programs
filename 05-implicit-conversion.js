70d68c37b6e75b09c7c3add25d4b392ebc1db376
SaiesShaikh07 <saiesshaikh007@gmail.com>
2024-08-07 12:32:56 +0530

    implicit conversion assignment
    implicit conversion assignment

---
+++ 05-implicit-conversion.js
@@ -0,0 +1,15 @@
+// numeric string used with + gives string type
+
+let result;
+
+result = '3' + 2;
+console.log("the result is = ",result) ;
+
+result = '3' + true;
+console.log("the result is = ",result); 
+
+result = '3' + undefined; 
+console.log("the result is = ",result); 
+
+result = '3' + null; 
+console.log("the result is = ",result); 
