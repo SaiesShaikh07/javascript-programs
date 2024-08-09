76c8415dacdf71554087784401ed08a3adee8df8
SaiesShaikh07 <saiesshaikh007@gmail.com>
2024-08-09 23:35:31 +0530

    for loop all with assignment

---
+++ 06-for-loop-assignment.js
@@ -0,0 +1,39 @@
+console.log("========================= Step 1 no. from 5 to 15 increment by 1 ============================");
+for (let i = 5; i <=15; i++) {
+    console.log(i);  
+}
+
+console.log("========================= Step 2 no. from 50 to 40 decrement by 1 ============================");
+for (let i = 50; i >=40; i--) {
+    console.log(i);  
+}
+
+console.log("========================= Step 3 no. first 15 odd numbers ============================");
+for (let i = 1; i<=30; i=i+2) {
+    console.log(i);  
+}
+
+console.log("========================= Step 4 no. first 10 even numbers ============================");
+for (let i = 2; i<=20; i+=2) {
+    console.log(i);  
+}
+
+
+console.log("========================= Step 5  Table of 5 ============================");
+for (let i = 5; i<=50; i+=5) {
+    console.log(i);  
+}
+
+console.log("========================= Step 6  Table of 10 ============================");
+for (let i = 10; i<=100; i+=10) {
+    console.log(i);  
+}
+
+console.log("========================= Step 7  Table of 10 in reverse order ============================");
+for (let i = 100; i>=10; i-=10) {
+    console.log(i);  
+}
+
+console.log(`++++++++++++++++++++++++++++++++++++++ E N D +++++++++++++++++++++++++++++++++++++++++++++++++++`);
+
+
