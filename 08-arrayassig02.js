f3cedefabd2e41e073e199e67fbf9eae20df7e29
SaiesShaikh07 <saiesshaikh007@gmail.com>
2024-08-14 12:45:53 +0530

    updated 08-arrayassig02.js
    updated 08-arrayassig02.js

--- 08-arrayassig02.js
+++ 08-arrayassig02.js
@@ -13,8 +13,7 @@ console.log(`the third last element =`,result);
 console.log("                                                            ");
 
 console.log(`======================== 4. Find the all even numbers using for of loop ==============================`);
-const array1 = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
-for (const element of array1) {
+for (const element of array) {
     if (element%2==0) {
       console.log(`Even number in given array is =`,element); 
     }   
@@ -22,20 +21,52 @@ for (const element of array1) {
 console.log("                                                            ");
 
 console.log(`======================== 5. Find the odd numbers for of loop ==============================`);
-const array2 = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
-for (const element of array1) {
+
+for (const element of array) {
     if (element%2==1) {
       console.log(`Odd number in given array is =`,element); 
     }   
 }
 console.log("                                                            ");
 
-console.log(`************************ 6 , 7 , 8 ignore ************************ `);
+console.log(`======================== 6.Find all the even positioned elements from array Numbers, sum it and log on console  ==============================`);
+var add=0;
+for (let index = 0; index < array.length; index++) {
+  if (index%2==0) {
+    const element = array[index];
+    add=add+element;
+
+  }
+  
+}
+console.log(`Even positioned elements addition is = `,add);
+
+console.log("                                                            ");
+
+console.log(`======================== 7.Find all the odd positioned elements from array Numbers, sum it and log on console  ==============================`);
+var add=0;
+for (let index = 0; index < array.length; index++) {
+  if (index%2==1) {
+    const element = array[index];
+    add=add+element;
+
+  }
+  
+}
+console.log(`Odd positioned elements addition is = `,add);
+console.log("                                                            ");
+
+console.log(`======================== 8. Find the sum of all elements from arrayNumbers, log on console  ==============================`);
+let sum=0;
+for (const element of array) {
+  sum=sum+element;
+}
+console.log("Sum of all elements from array = ",sum);
 console.log("                                                            ");
 
 console.log(`======================== 9. Find the numbers which are multiple of 5 ==============================`);
-const array3 = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
-for (const element of array1) {
+
+for (const element of array) {
     if (element%5==0) {
       console.log(`multiple of 5 in given array is =`,element); 
     }   
@@ -43,11 +74,11 @@ for (const element of array1) {
 console.log("                                                            ");
 
 console.log(`======================== 10.Is number 115 available in array3 ? ==============================`);
-console.log(`Is number 115 available in array3 = `,array3.includes(115));
+console.log(`Is number 115 available in array3 = `,array.includes(115));
 console.log("                                                            ");
 
 console.log(`======================== 11.Is number 23 available in array3 ? ==============================`);
-console.log(`Is number 23 available in array3 = `,array3.includes(23));
+console.log(`Is number 23 available in array3 = `,array.includes(23));
 console.log("                                                            ");
 
 console.log(`======================== 12.Insert numbers → 55, 66 at index 3 and log  ==============================`);
@@ -59,7 +90,7 @@ console.log("                                                            ");
 console.log(`======================== 13. Delete 3 elements starting from index 4 and log array on console  ==============================`);
 const array5 = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
 let ind=array5.splice(4,3);
-console.log(`Deleted 3 elements starting from index 4 =`,ind);
+console.log(`Deleted 3 elements starting from index 4 =`,array5);
 console.log("                                                            ");
 
 
