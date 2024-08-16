3d9ec425d007121a39975cb8b882da5d12bc5bd1
SaiesShaikh07 <saiesshaikh007@gmail.com>
2024-08-16 13:33:37 +0530

    Assignment on object 2
    Assignment on object 2

---
+++ 09-object-assig-B.js
@@ -0,0 +1,56 @@
+
+const sbiBank = {
+    bankName:"State Bank of India",
+    location: "Mumbai",
+    accountNo:"1234567890",
+    ifsc: "SBIN0000001",
+    interestRate: "5.5%",
+    showDetails: function() {
+        console.log(`Bank Name: ${this.bankName} || `, `Location: ${this.location} || `, `Account No: ${this.accountNo} || `,` IFSC: ${this.ifsc} || `, `Interest Rate: ${this.interestRate}`);
+    }
+};
+
+
+const axisBank = {
+    bankName:"Axis Bank",
+    location: "Bangalore",
+    accountNo: "0987654321",
+    ifsc:"UTIB0000001",
+    interestRate: "6.0%",
+    showDetails: function() {
+        console.log(`Bank Name: ${this.bankName} || `, `Location: ${this.location} || `,` Account No: ${this.accountNo} || `, `IFSC: ${this.ifsc} || `, `Interest Rate: ${this.interestRate}`);
+    }
+};
+
+
+const hdfcBank = {
+    bankName:"HDFC Bank",
+    location: "Delhi",
+    accountNo: "1122334455",
+    ifsc: "HDFC0000001",
+    interestRate: "6.5%",
+    showDetails: function() {
+        console.log(`Bank Name: ${this.bankName} || `,` Location: ${this.location} || `,` Account No: ${this.accountNo} || `, `IFSC: ${this.ifsc} || `, `Interest Rate: ${this.interestRate}`);
+    }
+};
+
+const yesBank = {
+    bankName: "Yes Bank",
+    location: "Chennai",
+    accountNo: "6677889900",
+    ifsc: "YESB0000001",
+    interestRate: "7.0%",
+    showDetails: function() {
+        console.log(`Bank Name: ${this.bankName} || `,` Location: ${this.location} || `, `Account No: ${this.accountNo} || `, `IFSC: ${this.ifsc} || `, `Interest Rate: ${this.interestRate}`);
+    }
+};
+
+console.log("                                               ");
+sbiBank.showDetails();
+console.log("                                               ");
+axisBank.showDetails();
+console.log("                                               ");
+hdfcBank.showDetails();
+console.log("                                               ");
+yesBank.showDetails();
+console.log("                                               ");
